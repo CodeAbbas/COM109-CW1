2751c05db6b390edafd7a77d9b61a0ddfcb45b5c
Abbas <abbax.uddin@gmail.com>
2024-07-26 22:31:05 +0000

    Finalizing everything....

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
-// slider
-function supernova_slider() {
+// Gallery slider
+function gallery_slider() {
     let nextBtn = document.querySelector(".gallery .buttons .next"),
         prevBtn = document.querySelector(".gallery .buttons .prev"),
         slide = document.querySelectorAll(".gallery .photos .block"),
@@ -38,5 +38,82 @@ function supernova_slider() {
     }
 }
 
-supernova_slider();
-  
+gallery_slider();
+
+// Error handling for menu toggle
+const hamburger = document.querySelector(".hamburger");
+const navbarMenu = document.querySelector(".navbar-menu");
+
+if (hamburger && navbarMenu) {
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navbarMenu.classList.toggle("active");
+  });
+} else {
+  console.error("Error!");
+}
+
+// Error handling for dark mode toggle
+const toggleButton = document.querySelector('.dark-light');
+
+if (toggleButton) {
+  toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+  });
+} else {
+  console.error("Error!");
+}
+
+
+// Form
+const form = document.getElementById('contact-form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs() {
+    const nameValue = name.value.trim();
+    const emailValue = email.value.trim();
+    const messageValue = message.value.trim(); 
+
+    if(nameValue === '') {
+        setErrorFor(name, 'Name cannot be blank');
+    } else {
+        setSuccessFor(name);
+    }
+
+    if(emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Not a valid email');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if(messageValue === '') {
+        setErrorFor(message, 'Message cannot be blank'); 
+    } else {
+        setSuccessFor(message); 
+    }
+}
+
+function setErrorFor(input, message) {
+    const formGroup = input.parentElement;
+    const small = formGroup.querySelector('small');
+    formGroup.className = 'form-group error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formGroup = input.parentElement;
+    formGroup.className = 'form-group success';
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
